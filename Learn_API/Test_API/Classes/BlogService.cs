3a1f8200081a13932e05c10b5dc0b325032bf529
mkhoauit <92783675+mkhoauit@users.noreply.github.com>
2021-12-15 16:42:49 +0700

    Learn-API

---
+++ Learn_API/Test_API/Classes/BlogService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Test_API.Classes
+{
+    public static class BlogService
+    {
+        static List<Blog> Blogs { get; }
+        static int nextId = 14; 
+        static BlogService()
+        {
+            Blogs = new List<Blog>
+            {
+                new Blog { BlogId = 11, Url = "test.com", Rating = 4, IsDeleted = true },
+                new Blog { BlogId = 12, Url = "dev.com", Rating = 5, IsDeleted = false },
+                new Blog { BlogId = 13, Url = "learnAPI.com", Rating = 3, IsDeleted = false },
+            };
+        }
+
+        public static List<Blog> GetAll() => Blogs;
+
+        public static Blog? Get(int id) => Blogs.FirstOrDefault(p => p.BlogId == id);
+
+        public static void Add(Blog blog)
+        {
+            blog.BlogId = nextId++;
+            Blogs.Add(blog);
+        }
+
+        public static void Delete(int id)
+        {
+            var blog = Get(id);
+            if(blog is null)
+                return;
+            Blogs.Remove(blog);
+        }
+
+        public static void Update(Blog blog)
+        {
+            var index = Blogs.FindIndex(p => p.BlogId == blog.BlogId);
+            if(index == -1)
+                return;
+            Blogs[index] = blog;
+        }
+    }
+}
---
+++ Learn_API/Test_API/Controllers/BlogController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Test_API.Classes;
+
+namespace Test_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BlogController : Controller
+    {
+        public BlogController()
+        {
+        }
+        // GET ALL
+        [HttpGet]
+        public ActionResult<List<Blog>> GetAll() =>BlogService.GetAll();
+        
+        // GET ID
+        [HttpGet("{id}")]
+        public ActionResult<Blog> Get(int id)
+        {
+            var findID = BlogService.Get(id);
+            
+            if(findID is null)
+                return NotFound();
+            
+            return findID;
+        }
+        // POST
+        [HttpPost]
+        public IActionResult Create(Blog blog)
+        {
+            BlogService.Add(blog);
+            return CreatedAtAction(nameof(Create), new {id=blog.BlogId }, blog);
+        }
+        // PUT
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Blog blog)
+        {
+            if (id != blog.BlogId)
+                return BadRequest();
+
+            var existingBlog = BlogService.Get(id);
+            if(existingBlog is null)
+                return NotFound();
+
+            BlogService.Update(blog);           
+
+            return Content($"Succecfully update blog {blog.BlogId}, URL: {blog.Url}, Rating: {blog.Rating}");
+        }
+        // DELETE
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var delBlog = BlogService.Get(id);
+
+            if (delBlog is null)
+                return NotFound();
+
+            BlogService.Delete(id);
+
+            return Content($"Succecfully DELETE blog {id}");
+        }
+    }
+}
